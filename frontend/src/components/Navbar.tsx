1962612845325767b45b718f93aa8cafb6894afa
salsabeel10 <salsabeelibrahim327@gmail.com>
2025-02-13 13:06:12 +0400

    fixed ui and add function fully implemented

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,23 +1,26 @@
 import { Link } from 'react-router-dom'
-
+import { FaPlusCircle } from 'react-icons/fa'
 
 const Navbar = () => {
-    
   return (
     <div className="navbar bg-base-100 shadow-sm">
-      <div className="flex-1">
-        <Link to={"/"}>
-        <p className="btn btn-ghost text-xl">Blog App</p>
-        </Link>
+      <div className="flex-1 ">
+        <ul className="menu menu-horizontal">
+            <Link to={'/'}>
+          <li className="border border-gray-400 rounded-4xl">
+              <p className="btn btn-ghost text-xl">Blog App</p>
+          </li>
+            </Link>
+        </ul>
       </div>
       <div className="flex-none">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <Link to={"/add"}>
-            <p className='text-lg'>Add Post</p>
+        <ul className="menu menu-horizontal">
+          <li className="border border-gray-400 rounded-4xl">
+            <Link to={'/add'}>
+              <p className="text-lg ">Add Post</p>
+              <FaPlusCircle size={18} />
             </Link>
           </li>
-          
         </ul>
       </div>
     </div>
--- frontend/src/components/PostList.tsx
+++ frontend/src/components/PostList.tsx
@@ -14,43 +14,48 @@ const PostList = () => {
     <div className="flex justify-center items-center min-h-screen pb-32">
       <div className="w-full max-w-lg rounded-lg p-6">
         <h1 className="text-xl font-bold text-center mb-4 underline">Posts</h1>
-
         {/* Show loading UI when fetching */}
         {loading ? (
           <div className="flex justify-center items-center py-10">
             <span className="loading loading-spinner loading-lg"></span>
           </div>
         ) : (
-          <ul className="w-full min-h-[300px] bg-base-100 border border-gray-200 rounded-box shadow-lg p-4">
-            {posts.map((post, index) => (
-              <li
-                key={post.id}
-                className="list-row flex items-center justify-between p-4 border-b last:border-none"
-              >
-                <div className="text-lg font-semibold text-gray-700 w-8 text-center">
-                  {String(index + 1).padStart(2, '0')}
-                </div>
-                <div className="text-center flex-1">
-                  <div className="font-extrabold text-lg uppercase">
-                    {post.title.slice(0, 10)}
+          <ul className="w-full bg-base-100 border border-gray-200 rounded-box shadow-lg p-4">
+            {posts.length > 0 ? (
+              posts.map((post, index) => (
+                <li
+                  key={post.id}
+                  className="list-row flex items-center justify-between p-4 border-b last:border-none"
+                >
+                  <div className="text-lg font-semibold text-gray-700 w-8 text-center">
+                    {String(index + 1).padStart(2, '0')}
                   </div>
-                  <div className="text-xs font-semibold opacity-60">
-                    {post.body.slice(0, 30)}...
+                  <div className="text-center flex-1">
+                    <div className="font-extrabold text-lg uppercase">
+                      {post.title.slice(0, 10)}
+                    </div>
+                    <div className="text-xs font-semibold opacity-60">
+                      {post.body.slice(0, 30)}...
+                    </div>
                   </div>
-                </div>
-                <div className="flex gap-2">
-                  <button
-                    className="btn btn-sm btn-ghost text-red-500"
-                    onClick={() => deletePost(post.id)}
-                  >
-                    <MdDelete size={18} />
-                  </button>
-                  <button className="btn btn-sm btn-ghost text-blue-500">
-                    <FaPen size={16} />
-                  </button>
-                </div>
-              </li>
-            ))}
+                  <div className="flex gap-2">
+                    <button
+                      className="btn btn-sm btn-ghost text-red-500"
+                      onClick={() => deletePost(post.id)}
+                    >
+                      <MdDelete size={18} />
+                    </button>
+                    <button className="btn btn-sm btn-ghost text-blue-500">
+                      <FaPen size={16} />
+                    </button>
+                  </div>
+                </li>
+              ))
+            ) : (
+              <div className="flex justify-center items-center py-10">
+                <span className="text-gray-500">No posts available.</span>
+              </div>
+            )}
           </ul>
         )}
       </div>
